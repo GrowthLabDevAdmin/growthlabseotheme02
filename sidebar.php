1d2305de4a11a5bd98075af93963b983dd7fee91
ArturoValv <art.valverde.563@gmail.com>
2026-03-02 20:07:46 -0600

    feat: addded dynamic sidebars per language

--- sidebar.php
+++ sidebar.php
@@ -6,7 +6,7 @@ if (!defined('ABSPATH')) {
 
 <aside class="sidebar <?= $args["classes"] ?>">
 
-    <?php dynamic_sidebar("sidebar-default") ?>
+    <?php dynamic_sidebar("sidebar-default" . get_current_language()["slug"] !== "" ? "-" . get_current_language()["slug"] : "") ?>
 
     <?php
     if (get_field("menus", $args["ID"]) && have_rows("menus", $args["ID"])):
