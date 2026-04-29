7f084b84955ec52ea683db45e25d3f26e9ae805d
ArturoValv <art.valverde.563@gmail.com>
2026-04-28 20:31:31 -0600

    fix: fixed company footer logo

--- footer.php
+++ footer.php
@@ -176,7 +176,7 @@
           ?>
 
           <a href="https://growthlabseo.com/" target="_blank" class="copyright-footer__logo" aria-label="Growth Lab SEO">
-            <img src="<?= get_stylesheet_directory_uri() . "/assets/img/Growth-Lab-Logo.png" ?>" alt="Growth Lab SEO Logo" width="270" height="50">
+            <img src="<?= get_template_directory_uri() . "/assets/img/Growth-Lab-Logo.png" ?>" alt="Growth Lab SEO Logo" width="270" height="50">
           </a>
 
         </div>
