fc8e05bc3817f73f1f24333ea58786416484617c
ArturoValv <art.valverde.563@gmail.com>
2026-05-27 18:59:10 -0600

    fix: block_style_attribute

--- blocks/block-content-box-grid/block.php
+++ blocks/block-content-box-grid/block.php
@@ -9,7 +9,7 @@ if (get_field('toggle_block')):
 
     <section
         id="<?= esc_attr($block_id ?? '') ?>"
-        class="block content-box-grid bg-bicolor<?= !empty($block['className']) ? ' ' . esc_attr($block['className']) : '' ?>"
+        class="block content-box-grid <?= !empty($block['className']) ? ' ' . esc_attr($block['className']) : '' ?>"
         <?= block_style_attribute($block); ?>
         <?php if (isset($extract_block_from_content) && $extract_block_from_content) echo "data-extract='$place'"; ?>>
 
